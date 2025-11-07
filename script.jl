94b460e73b2e384bf9fde996e02cd656a2b084ba
Mason Protter <mason.protter@icloud.com>
2025-11-07 18:04:30 +0100

    I think the filewatcher didn't error fast enough

--- script.jl
+++ script.jl
@@ -7,8 +7,13 @@ end
 
 @testset "plt" begin
     xs = 1:0.01:10
-    ys = sin.(xs) .+ rand() / 5
+    ys = sin.(xs) .+ rand() / 2
     plt = plot(xs, ys)
     @test_reference "imgs/noisy_sin.png" plt by=psnr_equality(50)
+    
+end
+
+@testset "Did the folder update?" begin
+    sleep(0.5)
     @test !istaskfailed(reference_test_watcher)
 end
