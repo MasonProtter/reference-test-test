8ab8a822642e2afa139cc409b1c735f0e977d1fc
Mason Protter <mason.protter@icloud.com>
2025-11-07 18:06:01 +0100

    oops

--- script.jl
+++ script.jl
@@ -7,7 +7,7 @@ end
 
 @testset "plt" begin
     xs = 1:0.01:10
-    ys = sin.(xs) .+ rand() / 2
+    ys = sin.(xs) .+ rand.() ./ 5
     plt = plot(xs, ys)
     @test_reference "imgs/noisy_sin.png" plt by=psnr_equality(50)
     
